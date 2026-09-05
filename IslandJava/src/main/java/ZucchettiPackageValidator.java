95946507e7b713dd8ef777eaf77b613d545e12c2
alessandro <alessandrorenzi26@gmail.com>
2026-09-05 19:27:08 +0200

    Added ZucchettiPackageValidatorTest to feature/validator branch Test not passed

---
+++ IslandJava/src/main/java/ZucchettiPackageValidator.java
@@ -0,0 +1,5 @@
+public class ZucchettiPackageValidator {
+    public boolean isValid(String s) {
+        return false;
+    }
+}
--- IslandJava/src/test/java/ZucchettiPackageValidatorTest.java
+++ IslandJava/src/test/java/ZucchettiPackageValidatorTest.java
@@ -1,2 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ZucchettiPackageValidatorTest {
+
+    @Test
+    public void isAZipPackage() {
+        ZucchettiPackageValidator validator = new ZucchettiPackageValidator();
+        assertTrue(validator.isValid("zucchetti.zip"), "Should be .zip file");
+    }
 }
