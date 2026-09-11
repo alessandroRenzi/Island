b7b46e96e9dba5acd5ba1a990f7293eabca63367
alessandro <alessandrorenzi26@gmail.com>
2026-09-11 17:13:27 +0200

    Added test for null and empty string on ZucchettiPackageValidatorTest.java for isValid Method

--- IslandJava/src/test/java/ZucchettiPackageValidatorTest.java
+++ IslandJava/src/test/java/ZucchettiPackageValidatorTest.java
@@ -17,27 +17,39 @@ public class ZucchettiPackageValidatorTest {
     }
 
     @Test
-    public void isA7ZipPackage(){
+    public void AssertTrueWithA7ZipPackage(){
         ZucchettiPackageValidator validator = new ZucchettiPackageValidator();
         assertTrue(validator.isValid("zucchetti.7z"),"7Zip file should be accepted");
     }
 
     @Test
-    public void isATarGzPackage(){
+    public void AssertTrueWithATarGzPackage(){
         ZucchettiPackageValidator validator = new ZucchettiPackageValidator();
         assertTrue(validator.isValid("zucchetti.tar.gz"),"gz file should be accepted");
     }
 
     @Test
-    public void isNotAZipPackage(){
+    public void AssertFalseWithNotAZipPackage(){
         ZucchettiPackageValidator validator = new ZucchettiPackageValidator();
         assertFalse(validator.isValid("zucchetti.iso"),"iso file shouldn't be accepted");
+    }
+
+    @Test
+    public void AssertFalseWithNullFile(){
+       ZucchettiPackageValidator validator = new ZucchettiPackageValidator();
+       assertFalse(validator.isValid(null),"null file shouldn't be accepted");
+    }
+
+    @Test
+    public void AssertFalseWithBlankStringOfFile(){
+        ZucchettiPackageValidator validator = new ZucchettiPackageValidator();
+        assertFalse(validator.isValid(""),"Blank string pointing to  file shouldn't be accepted");
     }
 
     //test that path not contain the file
     @Test
     public void fileFromAbsolutePathNotExists(@TempDir Path tempDir) {
-        Path fileFromTmpDir =tempDir.resolve("notZucchetti.zip");
+        Path fileFromTmpDir = tempDir.resolve("notZucchetti.zip");
         ZucchettiPackageValidator validator = new ZucchettiPackageValidator();
         assertFalse(validator.exists(fileFromTmpDir.toAbsolutePath().toString()),"File not should being in that path");
     }
