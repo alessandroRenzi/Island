160257149838a29b99cdf29a14ca02509caa3e93
alessandro <alessandrorenzi26@gmail.com>
2026-09-11 12:28:56 +0200

    Added to ZucchettiPackageValidatorTest two test for checking existing file. Implemented new method **exists** in class **ZucchettiPackageValidator.java**

--- IslandJava/src/main/java/ZucchettiPackageValidator.java
+++ IslandJava/src/main/java/ZucchettiPackageValidator.java
@@ -1,10 +1,23 @@
 import org.apache.commons.io.FilenameUtils;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 public class ZucchettiPackageValidator {
-    public boolean isValid(String s) {
-        String extension = FilenameUtils.getExtension(s);
+
+    //method that check if file passed is an allowed kind of file
+    public boolean isValid(String fileNameWithPath) {
+        String extension = FilenameUtils.getExtension(fileNameWithPath);
         return Objects.equals(extension, "zip");
     }
+
+    //method that check if file passed is located in file system
+    public boolean exists(String fileNameWithPath) {
+        Path completePathWithFile = Paths.get(fileNameWithPath);
+        return Files.exists(completePathWithFile) && Files.isRegularFile(completePathWithFile);
+    }
+
 }
--- IslandJava/src/test/java/ZucchettiPackageValidatorTest.java
+++ IslandJava/src/test/java/ZucchettiPackageValidatorTest.java
@@ -1,4 +1,9 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -17,5 +22,21 @@ public class ZucchettiPackageValidatorTest {
     public void isNotAZipPackage(){
         ZucchettiPackageValidator validator = new ZucchettiPackageValidator();
         assertFalse(validator.isValid("zucchetti.iso"),".iso file shouldn't be accepted");
+    }
+
+    //test that path not contain the file
+    @Test
+    public void fileFromAbsolutePathNotExists(@TempDir Path tempDir) throws IOException {
+        Path fileFromTmpDir =tempDir.resolve("notZucchetti.zip");
+        ZucchettiPackageValidator validator = new ZucchettiPackageValidator();
+        assertFalse(validator.exists(fileFromTmpDir.toAbsolutePath().toString()),"File not should being in that path");
+    }
+
+    //test that path contain the file
+    @Test
+    public void fileFromAbsolutePathExists(@TempDir Path tempDir) throws IOException {
+        Path fileFromTmpDir = Files.createFile(tempDir.resolve("Zucchetti.zip"));
+        ZucchettiPackageValidator validator = new ZucchettiPackageValidator();
+        assertTrue(validator.exists(fileFromTmpDir.toAbsolutePath().toString()),"File should being in that path ");
     }
 }
